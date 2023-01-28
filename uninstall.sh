2e0f2d0e6dcfe16394538a0f11e5f03247d5b547
beefbad <123251487+beefbad@users.noreply.github.com>
2023-01-28 14:32:55 -0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo launchctl unload -w /Library/LaunchDaemons/com.dev.spoofMAC.plist
-sudo launchctl remove -w /Library/LaunchDaemons/com.dev.spoofMAC.plist
-sudo rm -rfv /Library/LaunchDaemons/com.dev.spoofMAC.plist
+sudo launchctl unload -w /Library/LaunchDaemons/com.beefbad.spoofMAC.plist
+sudo launchctl remove -w /Library/LaunchDaemons/com.beefbad.spoofMAC.plist
+sudo rm -rfv /Library/LaunchDaemons/com.beefbad.spoofMAC.plist
